d037deb1d854449785bb4b5b61baaef98ecca458
Dipesh <dipeshsharmagautam@outlook.com>
2025-06-18 14:12:40 +0545

    update a value

--- src/components/openSource/Contribution.tsx
+++ src/components/openSource/Contribution.tsx
@@ -1,7 +1,7 @@
 export function ContributionCard() {
   return (
     <div className="w-full h-64 flex justify-center items-center text-3xl text-[#3498db] dark:text-[#8a2be2] font-bold">
-      Coming Soon...
+      Coming Soon... (Please wait a moment, i'm trying to make a perfect resume.)
     </div>
   )
 }
